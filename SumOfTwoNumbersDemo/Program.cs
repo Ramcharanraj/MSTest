ba436ddebfb7519d3caf53a660d1a37d0dc18318
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-10-14 00:31:00 +0530

    ADDED: MSTesting SumOfTwoNumbersProblem

---
+++ SumOfTwoNumbersDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SumOfTwoNumbersDemo
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public int Add(int numOne, int numTwo)
+        {
+            var sum = numOne + numTwo;
+            return sum;
+        }
+    }
+}
---
+++ SumOfTwoNumbersMSTestUnitTesting/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SumOfTwoNumbersDemo;
+
+namespace SumOfTwoNumbersMSTestUnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly SumOfTwoNumbersDemo.Program program;
+
+        public UnitTest1()
+        {
+            program = new SumOfTwoNumbersDemo.Program(); 
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int valueOne = 5;
+            int valueTwo = 10;
+            var result = program.Add(valueOne, valueTwo);
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int valueOne = 59;
+            int valueTwo = 10;
+            var result = program.Add(valueOne, valueTwo);
+            Assert.AreEqual(15, result);
+        }
+
+
+    }
+}
